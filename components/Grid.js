9e51d8c96e27369f05ec662c82b469e0f8a265c5
robertgrzonka <hello@robertgrzonka.pl>
2019-01-12 23:49:41 +0100

    fixup! Merge branch 'master' of github.com:robertgrzonka/theguys

--- components/Grid.js
+++ components/Grid.js
@@ -1,14 +1,46 @@
-const Grid = (props) => (
-  <div className='container'>
+const col = {
+  height: 'auto',
+  display: 'flex',
+  margin: 5,
+  width: '100%'
+}
+
+export const Col = (props) => (
+  <div style={col}>
     {props.children}
-    <div className='row'>
-      {props.children}
-      {props.col}
-      <div className={this.props.col}>
-        {props.children}
-      </div>
-    </div>
   </div>
 )
 
-export default Grid
+const row = {
+  height: 'auto',
+  display: 'flex',
+  margin: 10
+}
+
+export const Row = (props) => (
+  <div style={row}>
+    {props.children}
+  </div>
+)
+
+const container = {
+  maxWidth: '100%',
+  height: 'auto',
+  margin: '50px',
+  padding: '50px',
+  display: 'grid'
+}
+
+export const Container = ({ children }) => (
+  <div style={container}>
+    {children}
+  </div>
+)
+
+export const Grid = (props) => (
+  <Container>
+    <Row>
+      {props.children}
+    </Row>
+  </Container>
+)
---
+++ pages/sandbox.js
@@ -0,0 +1,10 @@
+import { Container, Row, Col } from '../components/Grid'
+
+export default () => (
+  <Container>
+    <Row>
+      <Col>Hello</Col>
+      <Col>Again</Col>
+    </Row>
+  </Container>
+)
