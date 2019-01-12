c8599f9b200aebbcc90ada858c0043f6357c74e0
robertgrzonka <hello@robertgrzonka.pl>
2019-01-12 23:36:21 +0100

    fixup! fixup! style*

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+const Header = props => (
+  <h1 style={props}>
+    {props.text}
+    <span>
+      {props.children}
+    </span>
+    <style jsx>{`
+    h1 {
+      font-family: 'Cormorant Garamond', serif;
+      font-size: 2rem;
+      color: black;
+      margin: 10px 0 20px 0;
+      font-weight: 500;
+      letter-spacing: 0.1rem;
+      }
+  `}</style>
+  </h1>
+)
+
+export default Header
